b59b756c10b7197262bdbf15a8560cbc11e3c000
Abel Lucas <abelucas009@gmail.com>
2024-07-01 09:49:12 -0300

    ajustando importações necessárias

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from src import *
-import uvicorn
 
 #Home Page
 @app.get("/")
--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from src.database import dataBase
 from src.models import Athlete, Category
+import uvicorn
 
 app = FastAPI(title= "Athetles_Api")
 BD= dataBase()
